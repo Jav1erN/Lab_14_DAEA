782a5f163d45874ff81f017ec0f320d1a7e73e1c
Javier Neira <javier.neira@tecsup.edu.pe>
2026-06-30 16:08:39 -0500

    Ultimos cambios realizados compatibilidad (1/6)

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+614b865abc3cbae4f0c991ef7a05006d6d448adb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e68075c19f36d627df6b9556d544587278fe97d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
