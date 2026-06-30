4e68075c19f36d627df6b9556d544587278fe97d
Javier Neira <javier.neira@tecsup.edu.pe>
2026-06-30 15:58:22 -0500

    Insercion de modulos (2/6) Primera parte de modulos completos y modificaciones sencillas en el debug de la app

--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e280393d8d533e046d7e280ea3a85bf32070ee0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+614b865abc3cbae4f0c991ef7a05006d6d448adb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
